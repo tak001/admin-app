b2959ed19b0f358a040e340d88133ebd13a27eec
tak001 <twitwitwi2@yahoo.co.jp>
2020-12-13 03:45:00 +0900

    feat:社員データの構造を定義する

--- src/app/members/members.component.ts
+++ src/app/members/members.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Member } from '../members';
 @Component({
   selector: 'app-members',
   templateUrl: './members.component.html',
   styleUrls: ['./members.component.css'],
 })
 export class MembersComponent implements OnInit {
-  member = '田中';
+  member: Member = { id: 1, name: '田中太郎' };
 
   constructor() {}
 
