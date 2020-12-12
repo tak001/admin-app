20de8a17d98c1cc9760fbbba93702f71765df319
tak001 <twitwitwi2@yahoo.co.jp>
2020-12-13 03:58:13 +0900

    feat:社員データを編集可能にする

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -6,7 +7,7 @@ import { MembersComponent } from './members/members.component';
 
 @NgModule({
   declarations: [AppComponent, MembersComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
