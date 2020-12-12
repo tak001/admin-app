1f1ab52cf5ba9e9c17d64766399e339cb0f71d0b
tak001 <twitwitwi2@yahoo.co.jp>
2020-12-13 03:39:52 +0900

    feat:add members component

---
+++ src/app/members/members.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-members',
+  templateUrl: './members.component.html',
+  styleUrls: ['./members.component.css'],
+})
+export class MembersComponent implements OnInit {
+  member = '田中';
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
