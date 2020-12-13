4fa7c5e8bba90e46c654057e44a1ae120cd57cd2
tak001 <twitwitwi2@yahoo.co.jp>
2020-12-13 13:40:24 +0900

    ダッシュボードを追加する

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
 import { MembersComponent } from './members/members.component';
 
-const routes: Routes = [{ path: 'members', component: MembersComponent }];
+const routes: Routes = [
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'members', component: MembersComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
