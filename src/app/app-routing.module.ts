14f94afb4b943c435663dfc8ecc6cbe0779d58e5
tak001 <twitwitwi2@yahoo.co.jp>
2020-12-13 13:52:15 +0900

    詳細ページへのリンクを設定する

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { MemberDetailComponent } from './member-detail/member-detail.component';
 import { MembersComponent } from './members/members.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'dashboard', component: DashboardComponent },
+  { path: 'detail/:id', component: MemberDetailComponent },
   { path: 'members', component: MembersComponent },
 ];
 
--- src/app/members/members.component.ts
+++ src/app/members/members.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Member } from '../member';
 import { MemberService } from '../member.service';
-import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-members',
@@ -10,22 +9,11 @@ import { MessageService } from '../message.service';
 })
 export class MembersComponent implements OnInit {
   members?: Member[];
-  selectedMember?: Member;
 
-  constructor(
-    private memberService: MemberService,
-    private messageService: MessageService,
-  ) {}
+  constructor(private memberService: MemberService) {}
 
   ngOnInit(): void {
     this.getMembers();
-  }
-
-  onSelect(member: Member): void {
-    this.selectedMember = member;
-    this.messageService.add(
-      `MembersComponent: 社員データ(id=${member.id}が選択されました)`,
-    );
   }
 
   getMembers(): void {
