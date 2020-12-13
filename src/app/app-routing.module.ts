1a7ad63a5bf134c5c02b4494635dd10f62a39e2b
tak001 <twitwitwi2@yahoo.co.jp>
2020-12-13 13:19:13 +0900

    ルーティングを設定する

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MembersComponent } from './members/members.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'members', component: MembersComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
