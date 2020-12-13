3ba3a7a11e0d24e3234aa59514d9346209e81f4f
tak001 <twitwitwi2@yahoo.co.jp>
2020-12-13 10:30:52 +0900

    社員データ一覧を表示する

--- src/app/members/members.component.ts
+++ src/app/members/members.component.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-import { Member } from '../members';
+import { Member } from '../member';
+import { MEMBERS } from '../mock-members';
+
 @Component({
   selector: 'app-members',
   templateUrl: './members.component.html',
   styleUrls: ['./members.component.css'],
 })
 export class MembersComponent implements OnInit {
+  members = MEMBERS;
   member: Member = { id: 1, name: '田中太郎' };
 
   constructor() {}
---
+++ src/app/mock-members.ts
@@ -0,0 +1,14 @@
+import { Member } from './member';
+
+export const MEMBERS: Member[] = [
+  { id: 11, name: '武山 岳大' },
+  { id: 12, name: '駒倉 光輝' },
+  { id: 13, name: '長田 健太' },
+  { id: 14, name: '高藤 百合香' },
+  { id: 15, name: '浜崎 敬浩' },
+  { id: 16, name: '緑川 睦' },
+  { id: 17, name: '森谷 凛' },
+  { id: 18, name: '大槻 弘大' },
+  { id: 19, name: '岩野 宣弘' },
+  { id: 20, name: '佐々木 小次郎' },
+];
